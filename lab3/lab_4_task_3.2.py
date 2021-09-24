4b1d63825d0f84a7ecbbcb03702c42f7bbb3e7f7
Staritsyna <staritsyna.ai@phystech.edu>
2021-09-24 13:46:52 +0300

    Update lab_4_task_3.2.py
    end

--- lab3/lab_4_task_3.2.py
+++ lab3/lab_4_task_3.2.py
@@ -26,6 +26,7 @@ ellipse(screen,GRAY,(90,130,60,170))
 
 line(screen, BLACK,[140,160],[200,190],2)
 line(screen, BLACK,[95,160],[70,210],2)
+line(screen, BLACK,[50,110],[70,210],1)
 #legs
 line(screen, BLACK,[95,270],[80,340],2)
 line(screen, BLACK,[65,340],[80,340],2)
@@ -33,25 +34,23 @@ line(screen, BLACK,[65,340],[80,340],2)
 line(screen, BLACK,[143,270],[160,340],2)
 line(screen, BLACK,[175,340],[160,340],2)
 
-#icecream
-polygon(screen,YELLOW,[[30,170],[70,210],[80,160]])
-circle(screen,RED , (70,160),15)
-circle(screen,BROWN , (35,165),15)
-circle(screen,WHITE, (50,150),15)
-
-#hand
-line(screen, BLACK,[340,160],[305,140],2)
-line(screen, BLACK,[340,160],[370,140],2)
-
+#ball
+polygon(screen,RED,[[30,65],[50,110],[70,60]])
+circle(screen,RED , (40,57),13)
+circle(screen,RED , (60,57),13)
 #girl1
 polygon(screen,PINK,[[300,110],[260,270],[340,270]])
 circle(screen,LGRAY , (300,100),30)
 line(screen, BLACK,[200,190],[295,140],2)
-
-line(screen, BLACK,[380,90],[370,140],1)
-polygon(screen,RED,[[380,90],[350,50],[390,45]])
-circle(screen,RED , (380,45),12)
-circle(screen,RED , (360,50),12)
+#hand
+line(screen, BLACK,[350,160],[305,140],2)
+line(screen, BLACK,[350,160],[400,140],2)
+#sh
+line(screen, BLACK,[400,90],[400,140],1)
+polygon(screen,YELLOW,[[425,50],[375,50],[400,90]])
+circle(screen,BROWN , (385,40),15)
+circle(screen,RED , (415,40),15)
+circle(screen,WHITE , (400,30),15)
 #legs
 line(screen, BLACK,[280,270],[280,340],2)
 line(screen, BLACK,[280,340],[265,340],2)
@@ -62,12 +61,33 @@ line(screen, BLACK,[320,340],[335,340],2)
 #girl2
 polygon(screen,PINK,[[500,110],[460,270],[540,270]])
 circle(screen,LGRAY , (500,100),30)
-
+line(screen, BLACK,[400,140],[450,160],2)
+line(screen, BLACK,[495,140],[450,160],2)
+line(screen, BLACK,[600,190],[510,140],2)
 #legs
 line(screen, BLACK,[480,270],[480,340],2)
 line(screen, BLACK,[480,340],[465,340],2)
 line(screen, BLACK,[520,270],[520,340],2)
 line(screen, BLACK,[520,340],[535,340],2)
+
+#body2
+circle(screen,LGRAY , (680,100),30)
+ellipse(screen,GRAY,(650,130,60,170))
+
+line(screen, BLACK,[660,160],[600,190],2)
+line(screen, BLACK,[705,160],[730,210],2)
+#legs
+line(screen, BLACK,[705,270],[720,340],2)
+line(screen, BLACK,[735,340],[720,340],2)
+
+line(screen, BLACK,[657,270],[640,340],2)
+line(screen, BLACK,[625,340],[640,340],2)
+
+#icecream
+polygon(screen,YELLOW,[[770,170],[730,210],[720,160]])
+circle(screen,RED , (730,160),15)
+circle(screen,BROWN , (765,165),15)
+circle(screen,WHITE, (750,150),15)
 
 pygame.display.update()
 clock = pygame.time.Clock()
